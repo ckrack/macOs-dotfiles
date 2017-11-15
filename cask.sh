168f1a04bb30d0afcba555373333c43c1ac1369a
Clemens Krack <ckrack@gmail.com>
2017-11-15 23:31:14 +0100

    Add cask and mas
    Install apps from cask and mac app store.

---
+++ cask.sh
@@ -0,0 +1,49 @@
+# Install via cask
+
+# Browser
+brew cask install google-chrome
+brew cask install chrome-devtools
+
+# Messenger
+brew cask install skype
+brew cask install bitrix24
+brew cask install whatsapp
+
+# Media
+brew cask install iina
+brew cask install gimp
+brew cask install sip
+brew cask install imageoptim
+
+# Tools
+brew cask install ccleaner
+brew cask install spectacle
+brew cask install gas-mask
+brew cask install keka
+brew cask install itsycal
+brew cask install gpg-suite
+
+# Quicklook Plugins
+brew cask install quicklook-csv
+brew cask install quicklook-json
+brew cask install qlmarkdown
+brew cask install betterzipql
+brew cask install qlimagesize
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install suspicious-package
+brew cask install qlvideo
+
+# Office
+brew cask install openoffice
+brew cask install sublime-text
+brew cask install poedit
+
+# Development
+brew cask install sourcetree
+brew cask install filezilla
+brew cask install sequel-pro
+brew cask install iterm2
+brew cask install postman
+
+brew cask install java
---
+++ mas.sh
@@ -0,0 +1,12 @@
+# Sign in
+mas signin ckrack@gmail.com
+
+# Install from Mac App Store
+# caffeine
+# xnconvert
+# forklift
+# slack
+mas install 411246225 \
+            436203431 \
+            412448059 \
+            803453959
